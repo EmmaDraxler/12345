1ba23dc55bbd5038ac3e3966707c8796199075d1
EmmaDraxler <draxemma@kkg-do.eu>
2026-04-24 08:34:37 +0200

    Refactor game logic for mouse interactions

--- src/game.lua
+++ src/game.lua
@@ -28,16 +28,72 @@ function game.update (dt)
     zustand = 3
     end
 
-    if zeit > 600 and punkte < 10 then
+    if zeit > 60 and punkte < 10 then
     zustand = 4
     end
+    
+    if love.mouse.isDown(1) == true then
+       if loecher == 1 and HammerX > 365 and HammerX < 440 and HammerY > 410 and HammerY < 520 then
+          loecher = love.math.random ( 2,11)
+          punkte = punkte + 1
+       end
 
+        if loecher == 2 and HammerX > 6 and HammerX < 81 and HammerY > 315 and HammerY < 425  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 3 and HammerX > 245 and HammerX < 320 and HammerY > 309 and HammerY < 419  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 4 and HammerX > 271 and HammerX < 346 and HammerY > 215 and HammerY < 325  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 5 and HammerX > 246 and HammerX < 321 and HammerY > 134 and HammerY < 244  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 6 and HammerX > 83 and HammerX < 158 and HammerY > 154 and HammerY < 264  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 7 and HammerX > 553 and HammerX < 628 and HammerY > 265 and HammerY < 375  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 8 and HammerX > 707 and HammerX < 782 and HammerY > 266 and HammerY < 376  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 9 and HammerX > 695 and HammerX < 770 and HammerY > 156 and HammerY < 266  then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 10 and HammerX > 469 and HammerX < 544 and HammerY > 87 and HammerY < 197 then
+          loecher = love.math.random ( 1,11)
+          punkte = punkte + 1
+       end
+
+        if loecher == 11 and HammerX > 503 and HammerX < 578 and HammerY > 183 and HammerY < 293 then
+          loecher = love.math.random ( 1,10)
+          punkte = punkte + 1
+       end
+    end
 end
 
 function game.draw ()
     --love.graphics.print("Zeit: " .. math.floor(zeit *100)/100, 375, 0)
-    love.graphics.print((hammerX *100)/100, 0, 0)
-    love.graphics.print((hammerY *100)/100, 375, 0)
+    --love.graphics.print((hammerX *100)/100, 0, 0)
+    --love.graphics.print((hammerY *100)/100, 375, 0)
     love.graphics.draw(ambro , ambroX, ambroY)
     hammer (hammerX, hammerY)
     if loecher == 1 then
@@ -45,9 +101,7 @@ function game.draw ()
         ambroY = 410
     end
 
-    if love.mouse.isDown(1) == true then
-
-    end
+    
 
 
         if loecher == 2 then
